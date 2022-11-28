5c7d48c639828b3b2d426dd27d3d60bdf2bbd0cd
Nguyen Co Tuan Anh <coanh2710@gmail.com>
2022-11-28 15:11:16 +0700

    add final solution

--- LT/TaxiRouting.cpp
+++ LT/TaxiRouting.cpp
@@ -8,7 +8,6 @@ int visited[MAX];
 int f_min = 99999999;
 int load;
 int f;
-int startPoint[MAX];
 int customers;
 int min_distance = 9999999;
 
@@ -20,82 +19,55 @@ void init()
         for (int j = 0; j <= 2 * n; j++)
         {
             scanf("%d", &c[i][j]);
-            if(i!==j)
+            if (i != j)
                 min_distance = min(min_distance, c[i][j]);
         }
     }
-    startPoint[0] = 1;
 }
 bool check(int v)
 {
-    if (v == 0)
-        return true;
     if (visited[v]) // tham roi thi tra ve false
         return false;
-    if (load == 0) // k nhan dc tra ve false
-    {
-        if (v > n)
-            return false;
-    }
-    else // k tra dc thi tra ve false
-    {
-        if (v <= n)
-            return false;
-        else if (!visited[v - n])
-            return false;
-    }
-
+    // if (load == 0) // k nhan dc tra ve false
+    // {
+    //     if (v > n)
+    //         return false;
+    // }
+    // else // k tra dc thi tra ve false
+    // {
+    //     if (v <= n)
+    //         return false;
+    //     else if (!visited[v - n])
+    //         return false;
+    // }
     return true;
 }
-void solution()
+void solution(int v)
 {
-    f_min = min(f_min, f);
+    f_min = min(f_min, f + c[v][0]);
 }
 void Try(int k, int v)
 {
-    for (int i = startPoint[v]; i <= 2 * n; i++)
+
+    for (int i = 1; i <= n; i++)
     {
         if (check(i))
         {
-            f += c[v][i];
-            visited[i] = true;
-            if (i != 0)
-            {
-                customers++;
-                if (load == 0)
-                    load++;
-                else
-                {
-                    load--;
-                }
-            }
 
-            if (i == 0)
+            f += c[v][i];
+            f += c[i][i + n];
+            visited[i] = true;
+
+            if (k == n)
             {
-                if (customers == 2 * n)
-                {
-                    f += c[i][0];
-                    solution();
-                    f -= c[i][0];
-                }
-                else
-                    Try(k + 1, i);
+                solution(i + n);
             }
-            else
-                Try(k + 1, i);
+            else if (f + ((n - k) * 2 + 1) * min_distance < f_min)
+                Try(k + 1, i + n);
 
             visited[i] = false;
             f -= c[v][i];
-            if (i != 0)
-            {
-                customers--;
-                if (load == 0)
-                {
-                    load++;
-                }
-                else
-                    load--;
-            }
+            f -= c[i][i + n];
         }
     }
 }
